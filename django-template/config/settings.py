900fe8e653995bec295583da754af5735e699a29
65usami <kenichi@usammi65.com>
2019-10-05 15:02:01 +0900

    time zone to settings.py

--- django-template/config/settings.py
+++ django-template/config/settings.py
@@ -147,14 +147,11 @@ LOGGING = {
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
-
-USE_I18N = True
-
-USE_L10N = True
-
+TIME_ZONE = 'Asia/Tokyo'
 USE_TZ = True
 
+USE_I18N = True
+USE_L10N = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
